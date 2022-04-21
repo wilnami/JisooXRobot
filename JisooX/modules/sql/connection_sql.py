a2e6301f30afecafd5784809cc3b03009b9a792a
Wilna Hanami <101022475+wilnami@users.noreply.github.com>
2022-04-22 01:02:43 +0700

    2

--- JisooX/modules/sql/connection_sql.py
+++ JisooX/modules/sql/connection_sql.py
@@ -22,7 +22,7 @@ class ChatAccessConnectionSettings(BASE):
 
 class Connection(BASE):
     __tablename__ = "connection"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     chat_id = Column(String(14))
     def __init__(self, user_id, chat_id):
         self.user_id = user_id
